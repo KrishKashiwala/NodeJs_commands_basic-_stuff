e0bea39b6c576d2dd4aacc8084f66da3793080bf
krish kashiwala <krishkashiwala@gmail.com>
2021-05-11 11:29:41 +0530

    ejs updation

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require('express')
 const path = require('path')
 const app= express();
 app.set('view engine', 'ejs')
-app.set('view' , 'server')
+// for custom folder rather than views
+// app.set('views' , path.join(__dirname , './temp'))
 app.listen(3000)
 app.get('/' , (req , res) => {
-    res.sendFile(path.join(__dirname , './server/index.html'));
+    res.render('index')
 })
 
 app.get('/about' , (req , res) => {
-    res.sendFile(path.join(__dirname , './server/about.html'));
+    res.render('about')
 })
 app.get('/about-in' , (req , res) => {
     res.redirect('/');
@@ -17,7 +18,6 @@ app.get('/about-in' , (req , res) => {
 
 app.use( (req , res) => {
     setTimeout(() =>{
-
     res.sendFile(path.join(__dirname , './server/404page.html'));   
     } , 3000)
 
